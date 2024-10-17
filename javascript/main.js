63024210bfe277d6d6afa805826e2b49560ee3be
farhan1654 <farhan1654akbar@gmail.com>
2024-10-17 22:24:02 +0700

    menambahkan tampilan tentang kami

--- javascript/main.js
+++ javascript/main.js
@@ -29,3 +29,22 @@ document.querySelectorAll('.btnDetail').forEach(item => {
 
     });
 });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Ganti gambar setiap 3 detik
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1; // Mengatur indeks slide sesuai dengan tombol yang ditekan
+    showSlides();
+}
