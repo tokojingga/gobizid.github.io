85025bae2fda131fe449ac920db38f3643d583ef
farhan1654 <farhan1654akbar@gmail.com>
2024-10-29 23:11:14 +0700

    edit main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,38 +1,38 @@
 document.querySelectorAll('.btnDetail').forEach(item => {
     item.addEventListener('click', (e) => {
-        let parent = e.target.parentNode.parentNode;
+        let parent = e.target.closest('.card'); // Menggunakan closest untuk mencari elemen induk
 
         let gambar = parent.querySelector('.card-img-top').src;
         let harga = parent.querySelector('.harga').innerHTML;
         let judul = parent.querySelector('.card-text').innerHTML;
-        let deskripsi = parent.querySelector('.deskripsi') ? parent.querySelector('.deskripsi').innerHTML : 
-        '<i>Tidak ada informasi yang tersedia</i>';
-        
+        let deskripsi = parent.querySelector('.deskripsi') ? 
+            parent.querySelector('.deskripsi').innerHTML : 
+            '<i>Tidak ada informasi yang tersedia</i>';
 
         let tombolModal = document.querySelector('.btnModal');
         tombolModal.click();
 
         document.querySelector('.modalTitle').innerHTML = judul;
+
         let image = document.createElement('img');
         image.src = gambar;
-        image.classList.add('w-100')
+        image.classList.add('w-100');
+        
         document.querySelector('.modalImage').innerHTML = '';
         document.querySelector('.modalImage').appendChild(image);
         document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
         document.querySelector('.modalHarga').innerHTML = harga;
 
-
         const nohp = '+62 852-9989-9161';
         let pesan = `https://api.whatsapp.com/send?phone=${nohp}&text=Halo, saya ingin membeli produk ini ${gambar}`;
 
         document.querySelector('.btnBeli').href = pesan;
-
     });
 });
-let slideIndex = 0;
-showSlides();
 
+let slideIndex = 0;
 let currentSlide = 0; // Menyimpan indeks slide saat ini
+showSlides(currentSlide); // Menampilkan slide pertama
 
 function showSlides(n) {
     let slides = document.getElementsByClassName("mySlides");
@@ -76,9 +76,7 @@ document.querySelector('.carousel-control-prev').addEventListener('click', funct
     showSlides(currentSlide);
 });
 
-
-
 function plusSlides(n) {
     slideIndex += n - 1; // Mengatur indeks slide sesuai dengan tombol yang ditekan
-    showSlides();
+    showSlides(slideIndex); // Memperbaiki pemanggilan showSlides dengan slideIndex
 }
